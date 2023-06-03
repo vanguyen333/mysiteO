cdb6fc8057461be80ba9c7cc9cd02158d173819c
vanguyen333 <vanguyen333@yahoo.com>
2023-06-03 15:56:26 -0400

    added home,about,header components, css bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-
 import "./App.css";
+import Header from "./components/header/header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
-  return <h1 className="w-screen h-screen bg-red-300">Test tailwindcss</h1>;
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return <h1>vanguyendev</h1>;
+};
+
+export default Header;
