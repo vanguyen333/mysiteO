2c73f9ec0a7e53b1837ee52a9c9cf78779f14b25
vanguyen333 <vanguyen333@yahoo.com>
2023-06-05 20:58:45 -0400

    added section to home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,24 +1,29 @@
 import { Container } from "react-bootstrap";
 import "./home.css";
+import React from "react";
 
 const Home = () => {
   return (
-    <Container className="homecl">
-      <div className="jumbotron">
-        <h1 className="display-4">Hi, I'm Van.</h1>
-        <p className="lead">
-          I develop web applications, softwares and applications.
-        </p>
-        <hr className="my-4" />
-        <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">
-          Learn more
-        </a>
-      </div>
+    <Container>
+      <div className="homecl"></div>
     </Container>
+
+    // <Container className="homecl">
+    //   <div className="jumbotron">
+    //     <h1 className="display-4">Hi, I'm Van.</h1>
+    //     <p className="lead">
+    //       I develop web applications, softwares and applications.
+    //     </p>
+    //     <hr className="my-4" />
+    //     <p>
+    //       It uses utility classes for typography and spacing to space content
+    //       out within the larger container.
+    //     </p>
+    //     <a className="btn btn-primary btn-lg" href="#" role="button">
+    //       Learn more
+    //     </a>
+    //   </div>
+    // </Container>
   );
 };
 
