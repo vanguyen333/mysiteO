eb543348ecf003ff2f1bc79965a5365aa1935ffa
vanguyen333 <vanguyen333@yahoo.com>
2023-06-07 19:46:33 -0400

    added logo brad successfully

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./header.css";
+// import Blog from "../blog/Blog.jsx";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-// import { logoBlack } from "../assets";
+
+// import { logoBlack } from "public/imgs/tulip.png";
 
 const Header = () => {
   return (
     <Navbar expand="lg" variant="dark" className="headercl">
       <Container>
         <Navbar.Brand className="titlenav" href="#home">
-          {/* <img src={logoBlack} /> */}
-          vanguyendev
+          <img className="logome" src="public/imgs/v.png" />
+          anguyendev
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
