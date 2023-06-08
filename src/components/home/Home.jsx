0d7eeb29901d36915e5c308f4e30458cc4f6b835
vanguyen333 <vanguyen333@yahoo.com>
2023-06-07 22:46:38 -0400

    style image and h1 to be on the same line

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,33 @@
 import { Container } from "react-bootstrap";
-import "./home.css";
-import React from "react";
+import "./Home.css";
+// import React from "react";
+// import <link rel="preconnect" href="https://fonts.googleapis.com">
+// <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+// <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@700&family=Quicksand&display=swap" rel="stylesheet"></link>
 
 const Home = () => {
   return (
     <Container>
-      <div className="homecl"></div>
+      <div className="homecl">
+        <table>
+          <tr>
+            <th>
+              <img
+                src="public/imgs/appleDesktop.jpg"
+                className="lady"
+                alt="laptp-lady"
+              />
+            </th>
+            <th>
+              <h1>Hi, I'm Van.</h1>
+              <h2>
+                I am a full stack developer. I develop applications, software
+                and web applications.
+              </h2>
+            </th>
+          </tr>
+        </table>
+      </div>
     </Container>
 
     // <Container className="homecl">
