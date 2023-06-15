6cd904a90f06947e529d8838ad6bad74c9e37dd5
vanguyen333 <vanguyen333@yahoo.com>
2023-06-14 20:53:26 -0400

    edit files and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./components/header/header";
-import Home from "./components/home/Home";
-import Blog from "./components/blog/Blog";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
   return (
-    <div>
-      <Header />
-      <Home />
-      {/* <Blog /> */}
-    </div>
+    <RouterProvider router={router} />
+    // <div>
+    //   <Header />
+    //   <Home />
+    //   {/* <Blog /> */}
+    // </div>
   );
 }
 
