3c1e2fe5029e298fd3edba483f5fc8a754326590
vanguyen333 <vanguyen333@yahoo.com>
2023-06-20 15:45:16 -0400

    added paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,8 +18,37 @@ const About = () => {
           alt="metuplip"
         />
       </div>
-      <div className="content">
-        <h1>Hi, it's Van again.</h1>
+      <div className="content text-center pl-52 pr-52">
+        <h1 className="pt-10 pb-10">Hi there, it's Van.</h1>
+        <p className="pb-10">
+          Thank you for taking the time to visit my personal website. I am
+          delighted to introduce myself as a confident and accomplished
+          full-stack developer based in Ottawa, Canada.
+          <br />
+          <br />
+          As a recent graduate from Algonquin College with a specialization in
+          Computer Programming, I possess a wide range of skills in building web
+          applications, applications, and software solutions. During my
+          education, I had the privilege of working as a co-op intern web
+          developer at U+Education, an Education company, for a duration of four
+          months. In this role, I played a pivotal part in designing, updating,
+          and revamping the company's WordPress website, gaining valuable
+          hands-on experience.
+          <br />
+          <br /> Additionally, I have successfully managed my time as a Senior
+          Women's Lead for the renowned Old Navy Brand, where I honed my
+          customer service skills and led a team towards achieving sales goals.
+          I thrive on embracing opportunities and challenges, as they contribute
+          to my continuous growth in skills and knowledge.
+          <br />
+          <br />
+          Passionate about designing innovative technology products, I am
+          constantly eager to learn and apply new skills and talents to drive
+          your organization's success. With my dedication and expertise, I am
+          confident in making a significant contribution to your team and
+          helping your organization thrive. Once again, I appreciate your visit
+          and encourage you to explore my portfolio further.
+        </p>
       </div>
     </div>
   );
