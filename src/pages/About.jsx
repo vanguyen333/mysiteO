0be10840b3603c3aac0fa2f7d2ebc3f717383596
vanguyen333 <vanguyen333@yahoo.com>
2023-06-20 14:49:27 -0400

    added 3 pics to about with some styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,26 @@
 const About = () => {
   return (
     <div>
-      <h1>Hi, it's Van again.</h1>
-      <img className="h-80" src="imgs/me.jpeg" alt="metuplip" />
+      <div className="pt-20 flex justify-center gap-10 flex-row about">
+        <img
+          className="pt-10 border-none h-80"
+          src="imgs/bird.jpg"
+          alt="metuplip"
+        />
+        <img
+          className="border-none rounded-3xl h-96"
+          src="imgs/me.jpeg"
+          alt="metuplip"
+        />
+        <img
+          className="pt-10 border-none h-80"
+          src="imgs/bird.jpg"
+          alt="metuplip"
+        />
+      </div>
+      <div className="content">
+        <h1>Hi, it's Van again.</h1>
+      </div>
     </div>
   );
 };
