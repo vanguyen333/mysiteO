9162bc793d53fecc84292513edb3397636deb3dd
vanguyen333 <vanguyen333@yahoo.com>
2023-06-22 17:56:26 -0400

    done styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
 const Footer = () => {
   return (
-    <div className="footer h-32 pt-10 mt-32 text-center bg-indigo-200">
+    <div className="footer h-32 pt-10 mt-32 text-center bg-indigo-200 ">
       <h1>
         @Created by<strong> Van Nguyen </strong> - 2023
       </h1>
-      <a href="https://github.com/vanguyen333">
-        {" "}
-        <img
-          className="h-6 border-none object-center"
-          src="./imgs/github.svg"
-          alt="github"
-        />
-      </a>
-      <a href="https://www.linkedin.com/in/van-nguyen-2aa017224/">
-        {" "}
-        <img className="h-6 border-none" src="./imgs/linkedin.svg" />{" "}
-      </a>
+      <div className="logos flex gap-10 justify-center pt-6">
+        <a href="https://github.com/vanguyen333">
+          <img
+            className="h-6 border-none object-center"
+            src="./imgs/github.svg"
+            alt="github"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/van-nguyen-2aa017224/">
+          <img className="h-6 border-none" src="./imgs/linkedin.svg" />{" "}
+        </a>
+      </div>
     </div>
   );
 };
