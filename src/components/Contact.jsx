a6d3aac07dfd514e1d97f5f447ee84e330d2478d
vanguyen333 <vanguyen333@yahoo.com>
2023-06-24 20:53:32 -0400

    update form styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,58 @@
 const Contact = () => {
   return (
-    <div className="footer h-60 pt-10 mt-32 text-center">
-      <form action="submit">
-        <h1>
-          Contact <span>me</span>
-        </h1>
-        <label htmlFor="name">Name: </label>
-        <input className="border" type="text" name="name" id="name" />
-        <br />
-        <label htmlFor="email">Email:</label>
-        <input className="border" type="text" name="email" id="email" />
-        <br />
-        <label htmlFor="subject">Subject:</label>
-        <input className="border" type="text" name="subject" id="subject" />
-        <br />
-        <label htmlFor="message">Message:</label>
-        <textarea className="border" type="text" name="message" id="message" />
-        <br />
-        <button
-          type="submit"
-          className="border  rounded-md py-2 px-4 font-bold bg-sky-400 hover:bg-sky-700"
-        >
-          Send
-        </button>
+    <div className="contact  h-90 pt-10 mt-32 text-center pb-24 pt-24 object-center">
+      <h1>
+        Contact <span>me</span>
+      </h1>
+      <form className="w-full max-w-xs">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Name:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            placeholder="Name"
+            type="text"
+          />
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              placeholder="Email"
+              type="email"
+            />
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="message"
+              >
+                Your message:
+              </label>
+              <input
+                className=" block p-2.5 h-24 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="message"
+                placeholder="Message"
+                type="textarea"
+              />
+            </div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
