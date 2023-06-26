a75cb219ad96cb7fa203922f89636092c2299e30
vanguyen333 <vanguyen333@yahoo.com>
2023-06-25 22:53:03 -0400

    working on blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,9 @@
 import "./blog.css";
 import Header from "../components/Header";
+
 const About = () => {
   return (
-    <div>
+    <div className="bg-blue-300 h-96 mt-24">
       <h1>My Blog</h1>
     </div>
   );
