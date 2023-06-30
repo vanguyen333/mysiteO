fdad7590fcaf2dac4ffa6ba16d812e4747a67691
vanguyen333 <vanguyen333@yahoo.com>
2023-06-29 22:30:38 -0400

    changed form style, responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,46 @@ const Contact = () => {
       <h1 className="text-3xl pb-10 font-bold">
         Contact <span>me</span>
       </h1>
+      <form action="submit">
+        <div className="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10">
+          <div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
+            <div className="col-span-2 lg:col-span-1">
+              <input
+                type="text"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Name"
+              />
+            </div>
 
-      <form className="mx-9 px-32 pt-6 pb-8 mb-0 justify-center justify-items-center">
+            <div className="col-span-2 lg:col-span-1">
+              <input
+                type="text"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Email Address"
+              />
+            </div>
+
+            <div className="col-span-2">
+              <textarea
+                cols="30"
+                rows="8"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Message"
+              ></textarea>
+            </div>
+
+            <div className="col-span-2 text-right">
+              <button
+                type="submit"
+                className="py-3 px-6 bg-blue-500 text-white font-bold w-full sm:w-32"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+      {/* <form className="mx-9 px-32 pt-6 pb-8 mb-0 justify-center justify-items-center">
         <div className="mb-4">
           <label
             className="block text-gray-700  text-sm font-bold mb-2"
@@ -55,7 +93,7 @@ const Contact = () => {
             </button>
           </div>
         </div>
-      </form>
+      </form> */}
     </div>
   );
 };
