0e5deeaeb70350ff82906aa7d848a495468bf8e1
vanguyen333 <vanguyen333@yahoo.com>
2023-07-01 17:23:48 -0400

    sidebar works but overlap with navbar

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,12 @@
 import "./blog.css";
 import Header from "../components/Header";
+import Sidethanh from "../components/Sidethanh";
 
 const About = () => {
   return (
     <div className="bg-blue-300 h-96 mt-24">
       <h1>My Blog</h1>
+      <Sidethanh />
     </div>
   );
 };
