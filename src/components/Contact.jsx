89481990598e536949160d194598da6cc6058836
vanguyen333 <vanguyen333@yahoo.com>
2023-07-06 19:25:04 -0400

    update form, installed emailjs, https://www.emailjs.com/docs/examples/reactjs/

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact  flex flex-col  items-center h-90 pt-10 mt-0 text-center pb-24 pt-24">
       <h1 className="text-3xl pb-10 font-bold">
         Contact <span>me</span>
       </h1>
-      <form action="submit">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10">
           <div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
             <div className="col-span-2 lg:col-span-1">
@@ -13,6 +37,7 @@ const Contact = () => {
                 type="text"
                 className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
                 placeholder="Name"
+                name="user_name"
               />
             </div>
 
@@ -21,6 +46,7 @@ const Contact = () => {
                 type="text"
                 className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
                 placeholder="Email Address"
+                name="user_meail"
               />
             </div>
 
@@ -30,16 +56,23 @@ const Contact = () => {
                 rows="8"
                 className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
                 placeholder="Message"
+                name="message"
               ></textarea>
             </div>
 
             <div className="col-span-2 text-right">
-              <button
+              {/* <button
                 type="submit"
+                value="Send"
                 className="py-3 px-6 bg-blue-500 text-white font-bold w-full sm:w-32"
               >
-                Submit
-              </button>
+                Send
+              </button> */}
+              <input
+                type="submit"
+                value="Send"
+                className="py-3 px-6 bg-blue-500 text-white font-bold w-full sm:w-32"
+              />
             </div>
           </div>
         </div>
