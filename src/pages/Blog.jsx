3e1faa8098f47b23239bb4f975777e1dd5e9e6cf
vanguyen333 <vanguyen333@yahoo.com>
2023-07-18 22:39:21 -0400

    responsive added

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,39 +1,44 @@
-import "./blog.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const About = () => {
   return (
-    <div>
-      <h1 className="text-5xl text-left pl-10 pt-10  font-bold">
-        #1 Angela Yu: Empowering Women in Coding and Technology
-      </h1>
-      <img className="border-none pl-10 pt-10" src="imgs/AngelaYu.jpg" alt="" />
-      <div className="text-left pt-10 pl-10 pb-10 pr-96">
-        <p>
-          Started learning to code at the age of 12. Didn't like the way Space
-          Invaders game was, her dad told her to change it and started to teach
-          her how to code. she was hooked on programming since then through
-          building a project with her dad.
-          <br />
-          As on her twitter saying: "I'm not a female coder, I'm a coder who
-          happens to be female."
-          <br />
-          Getting over the barrier of negative thinkings towards her of being
-          Asian and a women, she instead focuses on things that she can change
-          to build up her career. Her hard work and dedication have paid off. As
-          now she is a renowned role modle for many young girls and women to
-          look up to.
-          <br />
-          Whenever mentioning Udemy, her name and courses are always on the
-          recommended list about web development, Python, iOS App Development,
-          and so on. Her coding programs have helped numerous of people land a
-          dream job
-        </p>
-        <h1 className="text-5xl text-left pl-10 pt-10  font-bold">
+    <div className="md:flex-row">
+      <div className="md:w-3/4 mx-auto px-4 py-6">
+        <h1 className="text-5xl font-bold pt-4">
+          #1 Angela Yu: Empowering Women in Coding and Technology
+        </h1>
+        <img className="border-none pt-4 w-50" src="imgs/AngelaYu.jpg" alt="" />
+        <div className="text-left pt-4">
+          <p>
+            Started learning to code at the age of 12. Didn't like the way Space
+            Invaders game was, her dad told her to change it and started to
+            teach her how to code. She was hooked on programming since then
+            through building a project with her dad.
+            <br />
+            As on her Twitter saying: "I'm not a female coder, I'm a coder who
+            happens to be female."
+            <br />
+            Getting over the barrier of negative thinkings towards her of being
+            Asian and a woman, she instead focuses on things that she can change
+            to build up her career. Her hard work and dedication have paid off.
+            As now she is a renowned role model for many young girls and women
+            to look up to.
+            <br />
+            Whenever mentioning Udemy, her name and courses are always on the
+            recommended list about web development, Python, iOS App Development,
+            and so on. Her coding programs have helped numerous people land a
+            dream job.
+          </p>
+        </div>
+        <h1 className="text-5xl font-bold pt-4">
           #2 Exploring Web 3.0: The Future of Online Technology
         </h1>
-        <img className="border-none" src="imgs/web123.jpeg" alt="web123" />
+        <img
+          className="border-none pt-4 w-full"
+          src="imgs/web123.jpeg"
+          alt="web123"
+        />
         <p>
           Image source:{" "}
           <u>
@@ -44,18 +49,18 @@ const About = () => {
         </p>
         <p>
           <br />
-          <strong>What exactly is web 1.0 ?</strong>
+          <strong>What exactly is Web 1.0?</strong>
           <br />
-          Web 1.0 is considered the first stage of the world wide web where
-          users has limited interaction with websites. During this era, websites
-          typically featured static content presented through simple HTML pages.
-          Users has limited opportunities to engage or interact on these early
-          websites.
+          Web 1.0 is considered the first stage of the World Wide Web where
+          users have limited interaction with websites. During this era,
+          websites typically featured static content presented through simple
+          HTML pages. Users have limited opportunities to engage or interact on
+          these early websites.
           <br />
           <br />
-          <strong> How about Web 2.0 ? </strong>
+          <strong>How about Web 2.0?</strong>
           <br />
-          The websites we are using basically are the Web 2.0, it refers to the
+          The websites we are using basically are Web 2.0. It refers to the
           21st-century Internet applications that have transformed the digital
           era in the aftermath of the dotcom bubble. The term Web 2.0 first came
           into use in 1999 as the Internet pivoted toward a system that actively
@@ -64,8 +69,9 @@ const About = () => {
           particularly transformed; in general, social media allows users to
           engage and interact with one another by sharing thoughts,
           perspectives, and opinions. Users can tag, share, tweet, and like.[1]
-          <br /> <br />
-          <strong> What is Web 3.0 ?</strong>
+          <br />
+          <br />
+          <strong>What is Web 3.0?</strong>
           <br />
           With the introduction of advanced technologies, Web 3.0 is expected to
           take over the Internet in the near future. Web 3.0 allows users to
@@ -74,7 +80,7 @@ const About = () => {
         <p>
           <br />
           <br />
-          References [1]what is web 2.0 : link:
+          References [1] what is Web 2.0 : link:
           https://www.investopedia.com/terms/w/web-20.asp
         </p>
       </div>
