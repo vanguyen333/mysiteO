a16b60ffc1ff967650094f26265df627a890d0db
vanguyen333 <vanguyen333@yahoo.com>
2023-07-21 22:19:29 -0400

    working on useState, name is printed out

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,21 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const About = () => {
+const Blog = () => {
+  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleButtonClick = () => {
+    setDisplayName(name);
+  };
   return (
     <div className="md:flex-row">
       <div className="md:w-3/4 mx-auto px-4 py-6">
+        <h1 className="text-5xl font-bold pt-4">#0 Quote of the day</h1>
+        <p>Discover what your today's quote is, have fun!</p>
         <h1 className="text-5xl font-bold pt-4">
           #1 Angela Yu: Empowering Women in Coding and Technology
         </h1>
@@ -39,6 +49,8 @@ const About = () => {
             className="border-solid border-gray-200 bg-gray-50 border-2 p-2 md:text-md sm:w-1/4 mb-4"
             placeholder="Name *"
             name="user_name"
+            value={name}
+            onChange={handleInputChange}
           />
           <textarea
             id="message"
@@ -46,9 +58,13 @@ const About = () => {
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Write your thoughts here..."
           ></textarea>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+          <button
+            onClick={handleButtonClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+          >
             Enter
           </button>
+          {displayName}
         </div>
 
         <h1 className="text-5xl font-bold pt-4">
@@ -125,4 +141,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Blog;
