5cb188ff818b32d0a849e7d843ead8529a96a9d3
vanguyen333 <vanguyen333@yahoo.com>
2023-07-22 23:00:36 -0400

    finished title for Canadian Citizenship responsive

--- src/pages/CanadianCitizenship.jsx
+++ src/pages/CanadianCitizenship.jsx
@@ -1,5 +1,23 @@
 const CanadianCitizenship = () => {
-  return <div className="canada">Let's prepare for Canadian Citizenship!</div>;
+  return (
+    <div className="">
+      <div className="header flex flex-row items-center justify-center gap-10 text-center pt-20">
+        <img
+          className="h-28"
+          src="../public/imgs/maple.png"
+          alt="maple_leave"
+        />
+        <h1 className="text-5xl font-bold  text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-700">
+          Let's prepare for Canadian Citizenship!
+        </h1>
+        <img
+          className="h-28"
+          src="../public/imgs/maple.png"
+          alt="maple_leave"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CanadianCitizenship;
