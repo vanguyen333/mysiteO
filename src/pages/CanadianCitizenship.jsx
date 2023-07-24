86493674e11efa8bdfe5446ed340cdb02c336c68
vanguyen333 <vanguyen333@yahoo.com>
2023-07-23 22:41:48 -0400

    added contents mucluc to the Canadian Citizenship

--- src/pages/CanadianCitizenship.jsx
+++ src/pages/CanadianCitizenship.jsx
@@ -1,7 +1,7 @@
 const CanadianCitizenship = () => {
   return (
     <div className="">
-      <div className="header flex flex-row items-center justify-center gap-10 text-center pt-20">
+      <div className="header flex flex-row items-center justify-center gap-10 text-center pt-20 pb-10">
         <img
           className="h-28"
           src="../public/imgs/maple.png"
@@ -15,6 +15,27 @@ const CanadianCitizenship = () => {
           src="../public/imgs/maple.png"
           alt="maple_leave"
         />
+      </div>
+      <div className="text-xl pl-24 pb-10">
+        1. Applying for Citizenship
+        <br />
+        2. Rights and Responsibilities of Citizenship
+        <br />
+        3. Who we are <br />
+        4. Canada's History <br />
+        5. How Canadian's Govern Themselves <br />
+        6.Federal Elections <br />
+        7. The Justice System <br />
+        8. Canadian Symbols <br />
+        9. Canada's Economy <br />
+        10. Canada's Regions <br />
+        <div className="inner pl-28 ">
+          The Atlantic Provinces <br />
+          Central Canada <br />
+          The Prairie Provinces <br />
+          The West Coast <br />
+          The Nothern Territories <br />
+        </div>
       </div>
     </div>
   );
