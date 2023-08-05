6f9a3f3af99d02d1cf813815afa57c5b528a9860
vanguyen333 <vanguyen333@yahoo.com>
2023-08-05 10:34:59 -0400

    edit blog3

--- src/BlogCompo/Blog3.jsx
+++ src/BlogCompo/Blog3.jsx
@@ -5,30 +5,47 @@ const Blog3 = () => {
         #3 How 'GRATEFUL' can lead to Happiness?
       </h1>
       <p className="pt-4">
-        Grateful itself is a positive thing. Whenever I start a day with the
-        first thinking of how grateful I am to what I have and will have will
-        always makes me feel happy and blessed.
+        Grateful itself is a positive affirmation. Commencing each day by
+        acknowledging my gratitude for present and future blessings consistently
+        engenders feelings of joy and a sense of being blessed.
         <br />
-        The reason is because when our mind feels grateful - which is a positive
-        state of mind, our body's circulation generate more positive thinking,
-        we attract more great things come to life. Good things originated from
-        good thoughts. <br />
-        <br />A significant research has been made from
-        <strong> Dr.Masaru Emoto </strong>
+        The reason is because embracing gratitude initiates a positive mental
+        state, our body stimuates a surge of optimistic thoughts within our
+        mind. This consequently influences our body's circulation, fostering and
+        environment to attracting more favorable outcomes. Positive ideas beget
+        positive outcomes. <br />
+        <br />A significant research has been made from{" "}
+        <a href="https://thewellnessenterprise.com/emoto/">
+          {" "}
+          <strong> Dr. Masaru Emoto </strong>
+        </a>
         regarding how water behaves when being exposed to thoughts and words.
         <br />
         <br />
         Water is frozen then pictures being taken of the resultant crystals
         before and after doing something on it for the purpose of comparision ,
-        and the result blows people away. He play diffirent kinds of music and
-        take photograpsh for each music type. Classical music generates
+        and the result blows people away. He played diffirent kinds of music and
+        took photograpsh for each music type. Classical music generated
         well-shaped crystals but heavy metal songs produced misshapen, irregular
-        cystals. Positive words being said on water produces beautiful crystals
-        and negative words produce the oppositive result.
+        cystals. Positive words being said on water produced beautiful crystals
+        and negative words produced the oppositive result.
         <br /> <br />
-        Below are some images of water effects on a specifica task.
+        Below are some images of water effects on a specific task.
+        <img className="pt-4" src="imgs/waterExperi.jpeg" alt="" />
         <br />
-        Source: https://betterbelieveit.net/masaru-emotos-experiments-water/
+        Source:{" "}
+        <a href=" https://betterbelieveit.net/masaru-emotos-experiments-water/">
+          Click Here
+        </a>
+        <br />
+        Image Source:{" "}
+        <a href="https://t2conline.com/wp-content/uploads/2022/12/main-qimg-839283f790c30a00ffe88ff9b83cb07b-lq.jpeg">
+          Click Here
+        </a>
+        <br />
+        We all know that the majority of our bodies consist of water, but little
+        do we realize that every single thought or word we generate impact our
+        bodies significantly.
       </p>
     </div>
   );
