7449020e515797c37676369785eb2cc88817652a
vanguyen333 <vanguyen333@yahoo.com>
2023-08-14 16:29:04 -0400

    edit projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,10 @@ const Projects = () => {
             src="imgs/weather.png"
             alt="checking weather"
           />
-          <p className="flex justify-center pt-7">Reactjs, API key</p>
+
+          <p className="flex justify-center pt-7">
+            React app using API key to check weather
+          </p>
           <div className="flex justify-center">
             <button className=" bg-blue-500 text-white font-bold my-4 py-2 px-4 rounded-full">
               <a href="https://eclectic-cat-41d5da.netlify.app/">
@@ -34,7 +37,9 @@ const Projects = () => {
             src="imgs/tindog.png"
             alt="tindog"
           />
-          <p className="flex justify-center pt-7">Responsive HTML, CSS</p>
+          <p className="flex justify-center pt-7">
+            Responsive Tindog web using HTML, CSS
+          </p>
           <div className="flex justify-center">
             <button className="bg-blue-500  text-white font-bold my-4  py-2 px-4 rounded-full">
               <a href="https://yummyapple123.github.io/tindog/">Open project</a>
@@ -47,7 +52,9 @@ const Projects = () => {
             src="imgs/randomquotes.png"
             alt="random quote "
           />
-          <p className="flex justify-center pt-7">React Js, API key</p>
+          <p className="flex justify-center pt-7">
+            React app generate random quotes using API key
+          </p>
           <div className="flex justify-center">
             <button className="bg-blue-500 text-white font-bold my-4  py-2 px-4 rounded-full">
               <a href="https://jocular-maamoul-1b0c23.netlify.app/">
