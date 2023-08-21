85e213157a85379ddd50e062fe40b0995e6bb6c8
vanguyen333 <vanguyen333@yahoo.com>
2023-08-21 13:02:22 -0400

    animation for header Looking for a developer works

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,14 @@
-import React from "react";
-// import "./header.css";
-// import Blog from "../blog/Blog.jsx";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Home from "../pages/Home";
-import Blog from "../pages/Blog";
-
-import Courses from "../pages/Courses";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import { logoBlack } from "public/imgs/tulip.png";
-
-// </BrowserRouter>
-
-const Header = () => {
+import ReactTyped from "react-typed";
+const Header = ({ text, delay, infinite }) => {
   return (
     <div className="headercl w-full h-120">
-      {/* <Navbar expand="lg" variant="dark">
-        <Container>
-          <Navbar.Brand className="titlenav" href="#home">
-            <img className="logome" src="public/imgs/v.jpg" />
-            vanguyendev
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto text-xl">
-              <Nav.Link href="#home">Home</Nav.Link>
-
-              <Nav.Link href="#blog">Blog</Nav.Link>
-
-              <Nav.Link href="#contact">Contact</Nav.Link>
-
-              <NavDropdown title="Courses" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">HTML</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">CSS</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  JavaScript
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">React JS</NavDropdown.Item>
-
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Java</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> */}
-      {/* d6f4f8 */}
       <div className="homecl pt-20">
         <h1 className=" h1headercl font-serif ui-serif font-semibold text-center hover:system-ui text-[#d6f4f8] text-6xl ">
-          Looking for a developer?
+          <ReactTyped
+            strings={["Looking for a developer?"]}
+            typeSpeed={100}
+            loop
+          />
         </h1>
         <h2 className="text-2xl text-[#ffffff] text-center py-5">
           I am a full stack developer. I develop applications, software and web
